f3fe8d6a6d1e7ddb61ae41cb6214c82d16a7c6c9
Tymur Morozov <radist@desant.biz>
2021-05-02 13:45:26 +0300

    camprute video

---
+++ index.js
@@ -0,0 +1,99 @@
+// @ts-nocheck
+let preview = document.getElementById("preview");
+let recording = document.getElementById("recording");
+let startButton = document.getElementById("startButton");
+let stopButton = document.getElementById("stopButton");
+let downloadButton = document.getElementById("downloadButton");
+let logElement = document.getElementById("log");
+let videoSelect = document.getElementById("videoSourceSelect");
+
+let recordingTimeMS = 5000;
+
+function log(msg) {
+  logElement.innerHTML += msg + "\n";
+}
+
+function wait(delayInMS) {
+  return new Promise((resolve) => setTimeout(resolve, delayInMS));
+}
+
+function startRecording(stream, lengthInMS) {
+  let recorder = new MediaRecorder(stream);
+  let data = [];
+
+  recorder.ondataavailable = (event) => data.push(event.data);
+  recorder.start();
+  log(recorder.state + " for " + lengthInMS / 1000 + " seconds...");
+
+  let stopped = new Promise((resolve, reject) => {
+    recorder.onstop = resolve;
+    recorder.onerror = (event) => reject(event.name);
+  });
+
+  let recorded = wait(lengthInMS).then(
+    () => recorder.state == "recording" && recorder.stop()
+  );
+
+  return Promise.all([stopped, recorded]).then(() => data);
+}
+
+function stop(stream) {
+  stream.getTracks().forEach((track) => track.stop());
+}
+
+startButton.addEventListener(
+  "click",
+  function () {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: true,
+        audio: true,
+      })
+      .then((stream) => {
+        preview.srcObject = stream;
+        downloadButton.href = stream;
+        preview.captureStream =
+          preview.captureStream || preview.mozCaptureStream;
+        return new Promise((resolve) => (preview.onplaying = resolve));
+      })
+      .then(() => startRecording(preview.captureStream(), recordingTimeMS))
+      .then((recordedChunks) => {
+        let recordedBlob = new Blob(recordedChunks, { type: "video/webm" });
+        recording.src = URL.createObjectURL(recordedBlob);
+        downloadButton.href = recording.src;
+        downloadButton.download = "RecordedVideo.webm";
+
+        log(
+          "Successfully recorded " +
+            recordedBlob.size +
+            " bytes of " +
+            recordedBlob.type +
+            " media."
+        );
+      })
+      .catch(log);
+  },
+  false
+);
+
+stopButton.addEventListener(
+  "click",
+  function () {
+    stop(preview.srcObject);
+  },
+  false
+);
+
+navigator.mediaDevices.enumerateDevices()
+.then(devices => {
+  devices.forEach((device, i) => {
+    let option = document.createElement('option');
+    option.value = device.deviceId;
+    if (device.kind === 'videoinput') {
+      option.text = device.label || 'Camera ' + (videoSelect.length + 1);
+      videoSelect.appendChild(option);
+    }
+  })
+})
+.catch(log);
+
